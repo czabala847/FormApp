16c9403a66f872a8858a2f329cbfcf0cf2eede3b
Carlos Zabala <czabala847@gmail.com>
2022-07-20 18:20:47 -0500

    Validaciones y referencias con viewChield

--- src/app/template/components/basics/basics.component.ts
+++ src/app/template/components/basics/basics.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
-  styleUrls: ['./basics.component.scss']
+  styleUrls: ['./basics.component.scss'],
 })
 export class BasicsComponent implements OnInit {
+  @ViewChild('myForm') myForm!: NgForm;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  save() {
+    console.log(this.myForm);
   }
 
+  nameValid(): boolean {
+    return (
+      this.myForm?.form.controls['product']?.invalid &&
+      this.myForm?.form.controls['product']?.touched
+    );
+  }
 }
