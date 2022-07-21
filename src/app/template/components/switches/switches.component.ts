627ac3ab9618c07c60054c223c56be8f35e360a9
Carlos Zabala <czabala847@gmail.com>
2022-07-21 11:34:11 -0500

    Terminando seccion formularios por template

--- src/app/template/components/switches/switches.component.ts
+++ src/app/template/components/switches/switches.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styleUrls: ['./switches.component.scss']
+  styleUrls: ['./switches.component.scss'],
 })
 export class SwitchesComponent implements OnInit {
+  person = {
+    gender: 'M',
+    notification: true,
+  };
 
-  constructor() { }
+  termsAndConditions: boolean = false;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
