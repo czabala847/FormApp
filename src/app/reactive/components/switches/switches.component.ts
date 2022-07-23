6911c5a20755aecc7dfd35de03a31c792ee603b4
Carlos Zabala <czabala847@gmail.com>
2022-07-22 23:01:45 -0500

    switches en formularios reactivos

--- src/app/reactive/components/switches/switches.component.ts
+++ src/app/reactive/components/switches/switches.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styleUrls: ['./switches.component.scss']
+  styleUrls: ['./switches.component.scss'],
 })
 export class SwitchesComponent implements OnInit {
+  switchesForm: FormGroup;
 
-  constructor() { }
+  person = {
+    gender: 'F',
+    notification: false,
+  };
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.switchesForm = this.formBuilder.group({
+      gender: ['M', Validators.required],
+      notification: [true, Validators.required],
+      terms: [false, Validators.requiredTrue],
+    });
   }
 
+  ngOnInit(): void {
+    this.switchesForm.patchValue({ ...this.person, terms: false });
+
+    this.switchesForm.valueChanges.subscribe(({ terms, ...rest }) => {
+      this.person = rest;
+    });
+  }
+
+  save() {
+    if (this.switchesForm.valid) {
+      const values = this.switchesForm.value;
+      delete values.terms;
+
+      this.person = values;
+
+      console.log('Guardado...');
+    }
+  }
 }
